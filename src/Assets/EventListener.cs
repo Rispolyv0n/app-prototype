cdd824e67732ed4253ba7ee18fdfae9509f589fc
kevinbird61 <kevinbird61@gmail.com>
2017-08-01 13:24:13 +0800

    Update app usage.
    * Download mechanism.(Still need to fix problem on mobile)

--- src/Assets/EventListener.cs
+++ src/Assets/EventListener.cs
@@ -7,6 +7,7 @@ public class EventListener : MonoBehaviour {
 	public Camera MainCamera;
 	// For Push button
 	public Button LoginBtn;
+	public Button DlBtn;
 	// For Status bar
 	public Text statusBar;
 	public int flag;
@@ -17,8 +18,12 @@ public class EventListener : MonoBehaviour {
 	void Start () {
 		print ("Event Listener here!");
 		flag = 0;
+		// Login btn
 		Button btn = LoginBtn.GetComponent<Button> ();
 		btn.onClick.AddListener (user_login);
+		// Download btn
+		Button dlbtn = DlBtn.GetComponent<Button> ();
+		dlbtn.onClick.AddListener (go_dl);
 	}
 
 	/* User Login */
@@ -26,8 +31,17 @@ public class EventListener : MonoBehaviour {
 		// Print out login => Change current text info
 		// StartCoroutine(sleep(1));
 		flag = 1;
+		limit = 90;
 		statusBar.text = "旭民進來囉!";
 		print ("Login !");
+	}
+
+	/* Go to download page */
+	void go_dl(){
+		flag = 2;
+		limit = 180;
+		statusBar.text = "download !";
+		print ("Download !");
 	}
 
 	IEnumerator sleep(int sec){
@@ -39,7 +53,12 @@ public class EventListener : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (flag == 1) {
-			rotateValue = new Vector3(0, ++count, 0);
+			rotateValue = new Vector3 (0, ++count, 0);
+			MainCamera.transform.eulerAngles = transform.eulerAngles + rotateValue;
+			if (count == limit)
+				flag = 0;
+		} else if(flag == 2) {
+			rotateValue = new Vector3 (0, ++count, 0);
 			MainCamera.transform.eulerAngles = transform.eulerAngles + rotateValue;
 			if (count == limit)
 				flag = 0;
---
+++ src/Assets/downloadFile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class downloadFile : MonoBehaviour {
+	public Text statusbar;
+	public Text progress;
+	public Button dl;
+	public Slider progressbar;
+
+	// Use this for initialization
+	void Start () {
+		// Btn event
+		Button btn = dl.GetComponent<Button> ();
+		btn.onClick.AddListener (fork_DL);
+		// setting max,min 
+		progressbar.maxValue = 100;
+		progressbar.minValue = 0;
+		// disable slider 
+		progressbar.enabled = false;
+	}
+
+	void fork_DL(){
+		StartCoroutine (get_DL());
+	}
+
+	IEnumerator get_DL(){
+		// yield return www; // waiting for completed
+		// using isDone() 
+		if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+			WWW download = new WWW ("https://kevin.imslab.org:4000/adf_g?id=1");
+			while (!download.isDone) {
+				float result = download.progress * 100;
+				// setting progress 
+				progress.text = Mathf.Floor (result).ToString () + "/100";
+				// setting slider (progress bar)
+				progressbar.value = result;
+				// print for mobile
+				statusbar.text = "[PC] Waiting ... " + Mathf.Ceil(download.progress*100).ToString() + "%";
+				yield return null;
+			}
+			statusbar.text = "Total size:" + (download.bytes.Length/(1024*1024)).ToString() + "MB";
+		} else if(Application.platform == RuntimePlatform.Android) {
+			WWW download = new WWW ("https://kevin.imslab.org:4000/adf_g?id=1");
+			yield return download;
+			/*while (!download.isDone) {
+				float result = download.progress * 100;
+				// setting progress 
+				progress.text = Mathf.Floor (result).ToString () + "/100";
+				// setting slider (progress bar)
+				progressbar.value = result;
+				// print for mobile
+				statusbar.text = "[Mobile] Waiting ... "+ Mathf.Ceil(download.progress*100).ToString() + "%";
+				yield return null;
+			}*/
+			statusbar.text = "Total size:" + download.bytes.Length.ToString();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
