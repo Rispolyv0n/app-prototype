16c9556f3ae32cc83657b78f286da642a382769c
kevinbird61 <kevinbird61@gmail.com>
2017-08-04 11:01:41 +0800

    Complete download mechanism.
    * Now can download on android device. (Without saving)

--- src/Assets/downloadFile.cs
+++ src/Assets/downloadFile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class downloadFile : MonoBehaviour {
 	public Text statusbar;
@@ -42,19 +43,34 @@ public class downloadFile : MonoBehaviour {
 			}
 			statusbar.text = "Total size:" + (download.bytes.Length/(1024*1024)).ToString() + "MB";
 		} else if(Application.platform == RuntimePlatform.Android) {
-			WWW download = new WWW ("https://kevin.imslab.org:4000/adf_g?id=1");
-			yield return download;
-			/*while (!download.isDone) {
+			/*UnityWebRequest download = UnityWebRequest.Get ("https://kevin.imslab.org:4000/adf_g?id=1");
+			while (!download.isDone) {
+				float result = download.downloadProgress * 100;
+				// setting progress 
+				progress.text = Mathf.Floor (result).ToString () + "/100";
+				// setting slider (progress bar)
+				progressbar.value = result;
+				// print for mobile
+				statusbar.text = "[PC] Waiting ... " + Mathf.Ceil(download.downloadProgress*100).ToString() + "%";
+				yield return null;
+			}
+			statusbar.text = "Total size:" + (download.downloadedBytes/(1024*1024)).ToString() + "MB";*/
+			Dictionary<string, string> header = new Dictionary<string, string> ();
+			string userAgent = "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebkit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36";
+			header.Add ("user-agent",userAgent);
+			// With android, using http instead of https
+			WWW download = new WWW ("http://kevin.imslab.org:4001/adf_g?id=1",null,header);
+			while (!download.isDone) {
 				float result = download.progress * 100;
 				// setting progress 
 				progress.text = Mathf.Floor (result).ToString () + "/100";
 				// setting slider (progress bar)
 				progressbar.value = result;
 				// print for mobile
-				statusbar.text = "[Mobile] Waiting ... "+ Mathf.Ceil(download.progress*100).ToString() + "%";
+				statusbar.text = "[ANDROID] Waiting ... " + Mathf.Ceil(download.progress*100).ToString() + "%";
 				yield return null;
-			}*/
-			statusbar.text = "Total size:" + download.bytes.Length.ToString();
+			}
+			statusbar.text = "Total size:" + (download.bytes.Length/(1024*1024)).ToString() + "MB";
 		}
 	}
 
